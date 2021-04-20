b74764edfbcf407e1a2f4653377c50eccff3cd87
Arjun-Siva <arjunfromtnj@gmail.com>
2021-04-20 17:34:25 +0530

    get possible numbers

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -10,7 +10,7 @@ class Grid extends Component{
     }
 
     initCells = () => {
-        var cells = new Array(9);
+        let cells = new Array(9);
         for (let index = 0; index < 9; index++) {
             cells[index] = new Array(9);            
         }
@@ -19,7 +19,6 @@ class Grid extends Component{
                 cells[row][col] = {
                     val : "",
                     unstable : false,
-                    fixed: false,
                     block: 3*Math.floor(row/3) + Math.floor(col/3),
                 }
             }
@@ -28,23 +27,28 @@ class Grid extends Component{
     }
 
     handleChange = (row,col,newVal) => {
-        var newArray = [];
+        let newArray = [];
 
         for (var i = 0; i < 9; i++)
             newArray[i] = this.state.cells[i].slice()
         
         newArray[row][col].val = newVal;
-        if(newVal < 1 || newVal > 9)
-            newArray[row][col].unstable = true;
 
-        var updatedMatrix = this.checkStability(newArray, row, col);
+        let updatedMatrix = this.checkAllCellsStability(newArray);
+        if((newVal < 1 || newVal > 9) && newVal!==""){
+            updatedMatrix[row][col].unstable = true;
+        }
         this.setState({cells: updatedMatrix});
     }
 
     checkStability = (newArray, row, col) => {
-        var block = newArray[row][col].block;
+        if (newArray[row][col].val === "") {
+            return newArray;
+        }
+
+        let block = newArray[row][col].block;
         //check entire row
-        for(var i = 0; i < 9; i++) {
+        for(let i = 0; i < 9; i++) {
             if(i===col)
                 continue;
 
@@ -55,7 +59,7 @@ class Grid extends Component{
         }
 
         //check entire column
-        for(var j = 0; j < 9; j++) {
+        for(let j = 0; j < 9; j++) {
             if(j===row)
                 continue;
 
@@ -66,8 +70,8 @@ class Grid extends Component{
         }
 
         //check block
-        for (var r = 0; r < 9; r++) {
-            for (var c = 0; c < 9; c++) {
+        for (let r = 0; r < 9; r++) {
+            for (let c = 0; c < 9; c++) {
                 if(r===row && c===col)
                     continue;
                 if(newArray[r][c].block === block && newArray[r][c].val === newArray[row][col].val){
@@ -80,6 +84,92 @@ class Grid extends Component{
         return newArray;
     }
 
+    checkAllCellsStability = (cellArray) => {
+        for(let i = 0; i < 9; i++) {
+            for (let j = 0; j< 9; j++) {
+                cellArray[i][j].unstable = false;
+            }
+        }
+
+        for(let row = 0; row < 9; row++) {
+            for(let col = 0; col < 9; col++){
+                cellArray = this.checkStability(cellArray, row, col);
+            }
+        }
+        return cellArray;
+    }
+
+    fixNumbers = () => {
+        let newArray = [];
+
+        for (var i = 0; i < 9; i++)
+            newArray[i] = this.state.cells[i].slice()
+    
+        for (let i = 0; i<9; i++) {
+            for (let j = 0; j<9; j++) {
+                if(newArray[i][j].val !== "")
+                    newArray[i][j].fixed = true;
+            }
+        }
+        this.setState({cells: newArray})
+    }
+
+    possibleNumbers = (tempArray, row, col) => {
+        let available = ["1","2","3","4","5","6","7","8","9"];
+        for (let i = 0; i < 9; i++) {
+            let index = available.indexOf(tempArray[row][i].val);
+            if (index > -1) {
+                available.splice(index, 1);
+            }
+        }
+        for (let i = 0; i < 9; i++) {
+            let index = available.indexOf(tempArray[i][col].val);
+            if (index > -1) {
+                available.splice(index, 1);
+            }
+        }
+
+        var block = tempArray[row][col].block;
+
+        for (let r = 0; r < 9; r++) {
+            for (let c = 0; c < 9; c++) {
+                if (tempArray[r][c].block === block) {
+                    let index = available.indexOf(tempArray[r][c].val);
+                    if (index > -1) {
+                        available.splice(index, 1);
+                    }
+                }
+            }
+        }
+
+        return available;
+    }
+
+    solvePuzzle = () => {
+        var cellArray = this.state.cells;
+        var cells_and_possible = {};
+        for (let i = 0; i < 9; i++) {
+            for (let j = 0; j < 9; j++) {
+                if(cellArray[i][j].val !== ""){
+                    continue;
+                }
+                else{
+                    let available = this.possibleNumbers(cellArray, i, j);
+                    
+                    try{
+                        cells_and_possible[i][j] = available;
+                    }
+                    catch(err){
+                        cells_and_possible[i] = {};
+                        cells_and_possible[i][j] = available;
+                    }
+                }
+            }
+        }
+
+        console.log(cells_and_possible);
+    }
+
     render() {
         return(
             <div>
@@ -89,7 +179,6 @@ class Grid extends Component{
                         {
                         [...Array(9).keys()].map( (c) => {
                             let details = {...this.state.cells[r][c]};
-                            //let y = r*10 + c;
                             return <Cell 
                                     row={r} 
                                     col={c}
@@ -101,6 +190,7 @@ class Grid extends Component{
                         </div>
                     })
                 }
+                <button onClick = {() => this.solvePuzzle()}>Solve</button>
             </div>
         )
     }
